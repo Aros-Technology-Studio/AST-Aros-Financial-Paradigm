6339d29ffc5822d6edac3d83ba0a93567c62d89e
qetevanarotato-star <qetevanarotato@gmail.com>
2025-12-06 17:07:06 +0300

    modify nodechain module

--- src/nodechain/nodechain.module.ts
+++ src/nodechain/nodechain.module.ts
@@ -4,11 +4,13 @@ import { NodeEntity } from './node.entity';
 import { ShardEntity } from './shard.entity';
 import { ConsensusEventEntity } from './consensus_event.entity';
 
+import { NodeMetricsService } from './node_metrics.service';
+
 @Module({
     imports: [
         TypeOrmModule.forFeature([NodeEntity, ShardEntity, ConsensusEventEntity]),
     ],
-    providers: [],
-    exports: [],
+    providers: [NodeMetricsService],
+    exports: [NodeMetricsService],
 })
 export class NodeChainModule { }

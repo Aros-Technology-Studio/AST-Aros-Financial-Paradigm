180befbbe246ed926548482bf78910a2fa689ba2
qetevanarotato-star <qetevanarotato@gmail.com>
2026-01-05 22:54:38 +0300

    modified:   src/nodechain_engine/nodechain_engine.module.ts

--- src/nodechain_engine/nodechain_engine.module.ts
+++ src/nodechain_engine/nodechain_engine.module.ts
@@ -1,10 +1,16 @@
 
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NodeChainService } from './nodechain.service';
 import { ShardingManager } from './sharding.manager';
 import { GossipSimulationService } from './gossip.simulation';
+import { NodeEntity } from './entities/node.entity';
+import { ExecutionSnapshotEntity } from './entities/execution_snapshot.entity';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([NodeEntity, ExecutionSnapshotEntity])
+    ],
     providers: [NodeChainService, ShardingManager, GossipSimulationService],
     exports: [NodeChainService],
 })

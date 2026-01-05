3864239235b3f99afbb8f5eb23b2c6c052fb633b
qetevanarotato-star <qetevanarotato@gmail.com>
2026-01-05 22:54:23 +0300

    added execution_snapshot and node entities

---
+++ src/nodechain_engine/entities/node.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, Index } from 'typeorm';
+import { NodeType } from '../consensus.types';
+
+@Entity('nodes')
+export class NodeEntity {
+    @PrimaryColumn()
+    id: string;
+
+    @Column({ type: 'enum', enum: NodeType })
+    type: NodeType;
+
+    @Column({ nullable: true })
+    ip: string;
+
+    @CreateDateColumn({ type: 'bigint' })
+    joinedAt: number;
+
+    @Column({ default: true })
+    isActive: boolean;
+
+    @Column({ type: 'jsonb', default: {} })
+    metrics: {
+        uptime: number;
+        batchesProposed: number;
+        batchesValidated: number;
+        missedVotes: number;
+    };
+}

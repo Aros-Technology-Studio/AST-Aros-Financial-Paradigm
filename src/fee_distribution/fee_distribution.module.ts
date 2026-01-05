37704bc64b8d5cd073ebeebe0772336dc8952062
qetevanarotato-star <qetevanarotato@gmail.com>
2026-01-05 20:10:28 +0300

    modified fee_distribution.module.ts

--- src/fee_distribution/fee_distribution.module.ts
+++ src/fee_distribution/fee_distribution.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EpochEntity } from './epoch.entity';
 import { DistributionLogEntity } from './distribution_log.entity';
+import { Transaction } from '../ledger/entities/transaction.entity';
+import { FeeDistributionService } from './fee_distribution.service';
+import { PoTEngineModule } from '../proof_of_transaction_engine/pot_engine.module'; // Adjust path if needed
+import { TokenModule } from '../token/token.module';
+import { NodeChainEngineModule } from '../nodechain_engine/nodechain_engine.module';
+import { FeeDistributionFraudPreventionService } from './fraud-prevention.service';
+import { SmartContractIntegration } from '../integration/smart_contract.integration';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([EpochEntity, DistributionLogEntity]),
+        TypeOrmModule.forFeature([EpochEntity, DistributionLogEntity, Transaction]),
+        PoTEngineModule,
+        TokenModule,
+        NodeChainEngineModule
     ],
-    providers: [],
-    exports: [],
+    providers: [FeeDistributionService, FeeDistributionFraudPreventionService, SmartContractIntegration],
+    exports: [FeeDistributionService],
 })
 export class FeeDistributionModule { }

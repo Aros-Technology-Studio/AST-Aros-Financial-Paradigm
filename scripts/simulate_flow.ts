563fc7a5d3ed4f02ada47c66789859e936c64e66
qetevanarotato-star <qetevanarotato@gmail.com>
2026-01-25 21:57:28 +0300

    modified:   scripts/simulate_flow.ts

--- scripts/simulate_flow.ts
+++ scripts/simulate_flow.ts
@@ -53,7 +53,6 @@ async function bootstrap() {
         logger.log(`    Mint Success! TxHash: ${mintResult.txHash}`);
 
         // Step 3: Governance
-        // Step 3: Governance
         logger.log('[3] Governance: Creating Proposal...');
         const proposal = await governance.createProposal('Increase Fees', 'Raise fees by 1%', validatorId, ProposalImpactLevel.MEDIUM);
         logger.log(`    Proposal Created: ${proposal.id}`);

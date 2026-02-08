ef67e3d740fda3acd7b4910ed9f8ead07236e59e
qetevanarotato-star <qetevanarotato@gmail.com>
2026-02-08 07:53:37 +0300

    modified:   src/value_circulation/value_circulation.module.ts

--- src/value_circulation/value_circulation.module.ts
+++ src/value_circulation/value_circulation.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { CirculationService } from './circulation.service';
+import { TokenModule } from '../token/token.module';
 
 @Module({
+    imports: [forwardRef(() => TokenModule)],
     providers: [CirculationService],
     exports: [CirculationService]
 })
